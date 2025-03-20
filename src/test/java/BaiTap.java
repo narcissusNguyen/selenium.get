67d5a418902d1990c96603b703c4ff6f718e1c55
narcissusNguyen <tientien.nguyen9539@gmail.com>
2025-03-20 22:27:43 +0700

    Day3

--- src/test/java/BaiTap.java
+++ src/test/java/BaiTap.java
@@ -4,23 +4,44 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BaiTap {
     @Test
-   void openBrowserWithDefaultMode(){
+    void openBrowserWithDefaultMode() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://brave.com/");
-        Assert.assertEquals(driver.getTitle(),"Selenium");
+        Assert.assertEquals(driver.getTitle(), "Selenium");
         //driver.quit();
     }
+
     @Test
-    void openBrowserWithHeadlessMode(){
+    void openBrowserWithHeadlessMode() {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--headless");
 
         WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get("https://brave.com/");
-        Assert.assertEquals(driver.getTitle(),"Selenium");
+        Assert.assertEquals(driver.getTitle(), "Selenium");
         //driver.quit();
     }
 
+    @Test
+    void openBrowserWithMobileViewMode() {
+        Map<String, Object> deviceMetrics = new HashMap<>();
+        deviceMetrics.put("width", 344);
+        deviceMetrics.put("height", 882);
+        Map<String, Object> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceMetrics", deviceMetrics);
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://brave.com/");
+        Assert.assertEquals(driver.getTitle(), "Selenium");
+        // driver.quit();
+
+    }
 }
