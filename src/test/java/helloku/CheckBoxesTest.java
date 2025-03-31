33ed6521e0f0de3567f39fbd849c0d36ea0b3bee
narcissusNguyen <tientien.nguyen9539@gmail.com>
2025-03-31 22:23:38 +0700

    Day 3 update

--- src/test/java/helloku/CheckBoxesTest.java
+++ src/test/java/helloku/CheckBoxesTest.java
@@ -1,4 +1,15 @@
 package helloku;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class CheckBoxesTest {
+    @Test
+    void openBrowserWithDefaultMode(){
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+    }
 }
--- src/test/java/helloku/TestAuto.java
+++ src/test/java/helloku/TestAuto.java
@@ -16,24 +16,38 @@ public class TestAuto {
         Assert.assertEquals(driver.getCurrentUrl(), "https://the-internet.herokuapp.com/secure");
         Assert.assertTrue(driver.findElement(By.id("flash")).getText().contains("You logged into a secure area!"));
         driver.findElement(By.cssSelector("[href='/logout']")).click();
-        // driver.quit();
-
+        driver.quit();
+    }
         // case 2 user name sai, pw sai
-        driver.findElement(By.name("username")).sendKeys("Aaaaa");
-        driver.findElement(By.id("password")).sendKeys("SuperSecretP");
-        driver.findElement(By.cssSelector("[type='submit']")).click();
-        Assert.assertEquals(driver.getCurrentUrl(), "https://the-internet.herokuapp.com/login");
-        Assert.assertTrue(driver.findElement(By.id("flash")).getText().contains("Your username is invalid!"));
+        @Test
+        void tc2() {
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://the-internet.herokuapp.com/login");
+            driver.findElement(By.name("username")).sendKeys("Aaaaa");
+            driver.findElement(By.id("password")).sendKeys("SuperSecretP");
+            driver.findElement(By.cssSelector("[type='submit']")).click();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://the-internet.herokuapp.com/login");
+            Assert.assertTrue(driver.findElement(By.id("flash")).getText().contains("Your username is invalid!"));
+            driver.quit();
+        }
 
-        driver.navigate().refresh();
         // case 3user name dung, pw sai
-        driver.findElement(By.name("username")).sendKeys("tomsmith");
-        driver.findElement(By.id("password")).sendKeys("SuperSecretP");
-        driver.findElement(By.cssSelector("[type='submit']")).click();
-        Assert.assertEquals(driver.getCurrentUrl(), "https://the-internet.herokuapp.com/login");
+        @Test
+        void tc3() {
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://the-internet.herokuapp.com/login");
+            driver.findElement(By.name("username")).sendKeys("tomsmith");
+            driver.findElement(By.id("password")).sendKeys("SuperSecretP");
+            driver.findElement(By.cssSelector("[type='submit']")).click();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://the-internet.herokuapp.com/login");
+            driver.quit();
+        }
 
-        driver.navigate().refresh();
         //case 4 user name sai, pw dung
+        @Test
+        void tc4() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/login");
         driver.findElement(By.name("username")).sendKeys("hello");
         driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
         driver.findElement(By.cssSelector("[type='submit']")).click();
